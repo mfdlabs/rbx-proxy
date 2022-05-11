c600a588766d0e6c48304af443387d3636e0ce85
Nikita Petko <petko@vmminfra.net>
2022-05-11 22:11:33 +0100

    RBXPRR-13 RBXPRR-14 RBXPRR-16: Concurrent logging, optional file logging, better log levels (#33)
    [SKIP CI][SKIP ANALYSIS]
    
    * RBXPRR-13: Concurrent Logging
    
    * RBXPRR-13 RBXPRR-14 RBXPRR-16: Add settings for logging
    
    * RBXPRR-13 RBXPRR-14 RBXPRR-16: Integrate concurrent logging and the rest
    
    * Fix crawler issue

--- src/lib/utility/webUtility.ts
+++ src/lib/utility/webUtility.ts
@@ -30,7 +30,7 @@ abstract class WebUtility {
    * A regex to match the request User-Agent as a potential bot.
    */
   private static readonly _crawlerRegex =
-    /[Ss]lurp|[Tt]eoma|Scooter|Mercator|MSNBOT|Gulliver|[Ss]pider|[Aa]rchiver|[Cc]rawler|[Bb]ot[) \/_-]|Mediapartners-Google|[Pp]ython-(?=urllib|requests)|c[uU][rR][lL]|wxWidgets|facebookexternalhit|PowerShell|DOSarrest|Feedfetcher|Roblox diag2|BingPreview|Jakarta|LuaSocket|VortaxiaWebflow|ADmantX|A6-Indexer|Dalvik|Roblox\/WinHttp|Roblox\/WinInet$|Java\/1|^Get Request$|XaxisSemanticsClassifier|compatible;\\s+Synapse|^Google favicon$|SkypeUriPreview|[Ll]ynx|[Uu]ptime\\.com|package http|^expo9|WebIndex|ogic[Mm]onitor|HitLeap|StatusCake|statuscake/gi;
+    /[Ss]lurp|[Tt]eoma|Scooter|Mercator|MSNBOT|Gulliver|[Ss]pider|[Aa]rchiver|[Cc]rawler|[Bb]ot[) \/_-]|Mediapartners-Google|[Pp]ython-(?=urllib|requests)|c[uU][rR][lL]|wxWidgets|facebookexternalhit|PowerShell|DOSarrest|Feedfetcher|Roblox diag2|BingPreview|Jakarta|LuaSocket|VortaxiaWebflow|ADmantX|A6-Indexer|Dalvik|Roblox\/WinHttp|Roblox\/WinInet$|Java\/1|^Get Request$|XaxisSemanticsClassifier|compatible;\\s+Synapse|^Google favicon$|SkypeUriPreview|[Ll]ynx|[Uu]ptime\\.com|package http|^expo9|WebIndex|ogic[Mm]onitor|HitLeap|StatusCake|statuscake/;
 
   /**
    * Generates a random UUIDv4 string.
