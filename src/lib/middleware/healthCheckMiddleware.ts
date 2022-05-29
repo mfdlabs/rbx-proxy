83fabe1a05fd8c337c1170c1a65c90f24dc4941f
Nikita Petko <petko@vmminfra.net>
2022-05-29 04:00:40 +0100

    RBXPRR-37 RBXPRR-29: Health Check Middleware
    This middleware’s primary function is to respond with a health check if
    the request path matches environment.healthCheckPath.
    
    This middleware will not execute if environment.useHealthCheckMiddleware
    is false.

---
+++ src/lib/middleware/healthCheckMiddleware.ts
@@ -0,0 +1,48 @@
+/*
+   Copyright 2022 Nikita Petko <petko@vmminfra.net>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/*
+    File Name: healthCheckMiddleware.ts
+    Description: A middleware that will check the health of the server.
+    Written by: Nikita Petko
+*/
+
+import logger from '@lib/utility/logger';
+import environment from '@lib/environment';
+import loadBalancerInfoResponder from '@lib/responders/loadBalancerInfoResponder';
+
+import { NextFunction, Request, Response } from 'express';
+
+export default class HealthCheckMiddleware {
+  /**
+   * Invokes the middleware.
+   * @param {Request} request The request object.
+   * @param {Response} response The response object.
+   * @param {NextFunction} next The next function to call.
+   * @returns {void} Nothing.
+   */
+  public static invoke(request: Request, response: Response, next: NextFunction): void {
+    if (!environment.useHealthCheckMiddleware) return next();
+
+    if (request.originalUrl.toLowerCase() === environment.healthCheckPath) {
+      logger.information('Request is a health check request, responding with health check page');
+
+      loadBalancerInfoResponder.invoke(response, true, true, true);
+      return;
+    }
+    next();
+  }
+}
