2306245a27fe5fffcbabd5c24411fd0d1d5ed564
Nikita Petko <petko@vmminfra.net>
2022-05-29 04:01:12 +0100

    RBXPRR-38 RBXPRR-29: Begin Timing Middleware
    This middleware’s primary function is to start the request’s timing
    function, and this value will be used later on in the pipeline to
    compute the amount of time it took to execute a request.

---
+++ src/lib/middleware/beginTimingMiddleware.ts
@@ -0,0 +1,39 @@
+/*
+   Copyright 2022 Nikita Petko <petko@vmminfra.net>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/*
+    File Name: beginTimingMiddleware.ts
+    Description: Begins timing for the request.
+    Written by: Nikita Petko
+*/
+
+import '@lib/extensions/express/request';
+
+import { NextFunction, Request, Response } from 'express';
+
+export default class BeginTimingMiddleware {
+  /**
+   * Invokes the middleware.
+   * @param {Request} request The request object.
+   * @param {Response} response The response object.
+   * @param {NextFunction} next The next function to call.
+   * @returns {void} Nothing.
+   */
+  public static invoke(request: Request, response: Response, next: NextFunction): void {
+    request.context.set('startTime', Date.now());
+    next();
+  }
+}
